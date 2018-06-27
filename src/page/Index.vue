95847653f0df08e443e897570045622e8e943610
xank <xank@qq.com>
2018-06-27 22:23:47 +0800

    att click

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -28,7 +28,7 @@
   <div>
     <ds-header :wheel="true" :line="false">
       <ds-tab @change="change" :wheel="true" v-model="type">
-        <ds-tab-wheel-item v-if="item.showTab" :key="item.id" :id="item.id" v-for="item in ATT_TYPE">{{item.name}}</ds-tab-wheel-item>
+        <ds-tab-item type="wheel" v-if="item.showTab" :key="item.id" :id="item.id" v-for="item in ATT_TYPE">{{item.name}}</ds-tab-item>
       </ds-tab>
       <div slot="focus-icon">
         <att-icon :name="selectedIcon"></att-icon>
@@ -42,7 +42,7 @@
     <div v-show="mode == 'list'">
       <div class="att-list-wrapper">
         <att-list>
-          <att-list-item v-for="(item, index) in activeList" :data="item" :wait="waits[item.aid]" :key="index"></att-list-item>
+          <att-list-item @click="handleAttClick" v-for="(item, index) in activeList" :data="item" :wait="waits[item.aid]" :key="index"></att-list-item>
         </att-list>
       </div>
       <!-- <ds-scroll class="att-list-wrapper"> -->
